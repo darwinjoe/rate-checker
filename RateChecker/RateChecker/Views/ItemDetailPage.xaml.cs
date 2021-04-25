533be585a115c145156898aecfe0f446bc9f34e3
joe <joe@ENVY>
2021-04-25 22:23:44 +1000

    Add project files.

---
+++ RateChecker/RateChecker/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RateChecker.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RateChecker.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
